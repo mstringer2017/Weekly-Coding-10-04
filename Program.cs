c5a0981e108b0451e4339897a81f1c2c85e8faf3
mstringer2017 <matthewstringer@yahoo.com>
2021-10-04 11:13:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Weekly_Coding_10_04
+{
+    class Program
+    {
+        static bool CannotCapture(int[,] grid)
+        {
+            bool result = true;
+            int height = grid.GetLength(0);
+            int width = grid.GetLength(1);
+            for (int x = 0; result && x < height; x++)
+            {
+                for (int y = 0; result && y < width; y++)
+                {
+                    if (grid[x,y] == 1)
+                    {
+                        if (x + 2 < height)
+                        {
+                            if (y + 1 < width)
+                                result = grid[x + 2, y + 1] == 0;
+                            if (result && y - 1 >= 0)
+                                result = grid[x + 2, y - 1] == 0;
+                        }
+                        if (result && x + 1 < height)
+                        {
+                            if (y + 2 < width)
+                                result = grid[x + 1, y + 2] == 0;
+                            if (result && y - 2 >= 0)
+                                result = grid[x + 1, y - 2] == 0;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CannotCapture(new int[,] {
+                { 0, 0, 0, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 1, 0, 1, 0 },
+                { 0, 1, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 1, 0, 0, 0 }
+                }));
+
+            Console.WriteLine(CannotCapture(new int[,] {
+                {1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 0, 0, 1, 0, 1, 0, 1},
+                {1, 0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1, 0, 1, 0},
+                {1, 0, 0, 1, 0, 0, 0, 1}
+                }));
+        }
+    }
+}
